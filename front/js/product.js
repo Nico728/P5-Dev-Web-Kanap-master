8063e82fa5284677927c456ee5e367f4de4fcd4e
Nicolas Séau <seau.nicolas@gmail.com>
2022-11-04 10:22:09 +0100

    Remplacement innerHTML par createElement page script

--- front/js/product.js
+++ front/js/product.js
@@ -6,16 +6,20 @@ console.log(id);
 
 //récupération des produits
 
-fetch('http://localhost:3000/api/products')
+fetch('http://localhost:3000/api/products/${id}')
 .then(function(res) {
   if (res.ok) {
     return res.json();
   }
 })
 .then(function(value) {
-  console.table(value);
-  kanaps(value);
+  let descriptionCanape = document.getElementById('description');
+  let prixCanape = document.getElementById('prix');
+  let imageCanape = document.createElement('img');
+  let titreCanape = document.querySelector('title');
+  
 })
+
 .catch(function(err) {
   console.log("erreur 404, sur ressource api :" + err);
   document.querySelector("titles").innerHTML = "<h1> erreur 404, not found</h1>";
--- front/js/script.js
+++ front/js/script.js
@@ -3,6 +3,7 @@
   fetch('http://localhost:3000/api/products')
   .then(function(res) {
     if (res.ok) {
+      console.log(res.json);
       return res.json();
     }
   })
@@ -17,17 +18,35 @@
 
  // Affichage des produits 
 
-function kanaps(accueil) {
-    let espaceCanapes = document.querySelector("#items");
-    for (let article of accueil) {
-      espaceCanapes.innerHTML += `<a href="./product.html?_id=${article._id}">
-      <article>
-        <img src="${article.imageUrl}" alt="${article.altTxt}">
-        <h3 class="productName">${article.name}</h3>
-        <p class="productDescription">${article.description}</p>
-      </article>
-    </a>`;
+function kanaps(value) {
+    for (let i = 0; i < value.length; i++) {
+
+      // création de la balise a
+      let lienItems = document.createElement("a");
+      document.querySelector("#items").appendChild(lienItems);
+      lienItems.href = "./product.html?_id=${value[i]._id}";
+      
+      //création de la balise article
+      let articleItems = document.createElement("article");
+      lienItems.appendChild(articleItems);
+      
+      // création de la balise img
+      let imgItems = document.createElement("img");
+      articleItems.appendChild(imgItems);
+      imgItems.src = value[i].imageUrl;
+      imgItems.alt = value[i].altTxt;
+      
+      // création de la balise h3
+      let h3Items = document.createElement("h3");
+      articleItems.appendChild(h3Items);
+      h3Items.classList.add("productName");
+      h3Items.textContent = value[i].name;
+      
+      // création de la balise p
+      let pItems = document.createElement("p");
+      articleItems.appendChild(pItems);
+      pItems.classList.add("productDescription");
+      pItems.textContent = value[i].description;
+
     }
   }
-
-
