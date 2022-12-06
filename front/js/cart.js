165f93e8cf7efa29505e102be6002394390a4a12
Nicolas Séau <seau.nicolas@gmail.com>
2022-12-06 16:55:27 +0100

    maj

--- front/js/cart.js
+++ front/js/cart.js
@@ -62,18 +62,18 @@ function affichageCanape(obj) {
     pCouleur.textContent = "Couleur : " + obj.color;
     divDescription.appendChild(pCouleur);
 
-// Création du prix du produit dans la balise divDescription
-    var productUnit = "";
+// Récupération du prix et création du prix du produit dans la balise divDescription
     fetch("http://localhost:3000/api/products/" + obj.id)
-        .then(response => response.json())
-        .then(async function (resultatAPI) {
-            productUnit = await resultatAPI;
-    // insertion P price
+        .then(function(res) {
+            if(res.ok) {
+                return res.json();
+            }
+        })
+        .then(function(obj) {
             const pPrix = document.createElement('p');
             pPrix.textContent = "Prix : " + obj.price + " €";
             divDescription.appendChild(pPrix);
-        })
-        .catch(error => alert("Erreur : " + error));
+        });    
 
 // Création d'une Div qui contient la quantité, de 1-100 produits et la suppression
     const divQuantiteSupp = document.createElement('div');
@@ -87,7 +87,7 @@ function affichageCanape(obj) {
 
 // Création d'une balise p pour la quantité
     const pQuantite = document.createElement('p');
-    pQuantite.textContent = "Qté : " + obj.quantity;
+    pQuantite.textContent = "Qté : ";
     divQuantite.appendChild(pQuantite);
 
 // Création d'un Input pour la gestion de la quantité
@@ -110,6 +110,39 @@ function affichageCanape(obj) {
     pSupp.classList.add("deleteItem");
     pSupp.textContent = "Supprimer";
     divSupp.appendChild(pSupp);
+
+    calculPrix(obj);
+}
+
+// Fonction qui permet de savoir si notre panier est vide, si il est vide alors on affiche panier vide sinon,
+// on gère la quantité et le prix
+function calculPrix(obj) {
+    if (tableauObjet == null) {
+        const creationP = document.createElement('p');
+        creationP.textContent = 'Votre panier est vide';
+        positionDetail.appendChild(creationP);
+        console.log("votre panier est vide")
+    } else { 
+        console.log("votre panier contient des articles")
+        // On gère la quantité
+        let totalQ = 0;
+        const totalQuantite = document.querySelector("#totalQuantity");
+        tableauObjet.forEach((obj) => {
+            const calculTotalQuantite = totalQ + obj.quantity;
+            totalQ = calculTotalQuantite;
+        })
+        console.log(totalQ);
+        totalQuantite.textContent = totalQ;
+        // On gère le prix
+        let totalP = 0;
+        const totalPrix = document.querySelector("#totalPrice");
+        tableauObjet.forEach((obj) => {
+            const calculTotalPrix = obj.price * obj.quantity;
+            totalP += calculTotalPrix;
+        })
+        console.log(totalP);
+        totalPrix.textContent = totalP;
+    }
 }
 
 
@@ -133,161 +166,13 @@ function affichageCanape(obj) {
 
 
 
-/*
-function afficheDetail(canape) {
-    const positionDetail = document.querySelector('#cart__items');
-
-// Création de la balise Article 
-    const articleDetail = document.createElement('article');
-    articleDetail.className = 'cart__item';
-    articleDetail.setAttribute('data-id', canape._id);
-    articleDetail.setAttribute('data-color', canape.color);
-    positionDetail.appendChild(articleDetail);
-
-// Création de la balise Div pour l'image
-    const divImg = document.createElement('div');
-    divImg.className = 'cart__item__img';
-    articleDetail.appendChild(divImg);
-
-// Création de la balise Image dans la div
-    const baliseImg = document.createElement('img');
-    baliseImg.setAttribute('src', canape.img);
-    baliseImg.setAttribute('alt', "Photographie d'un canapé");
-    divImg.appendChild(baliseImg);
-
-// Création de la balise Div qui contient les infos du canapé
-    const divGroupe = document.createElement('div');
-    divGroupe.className = 'cart__item__content';
-    articleDetail.appendChild(divGroupe);
-
-// Création d'une Div qui contient le nom et la couleur et le prix du produit
-    const divDescription = document.createElement('div');
-    divDescription.className = "cart__item__content__description";
-    divGroupe.appendChild(divDescription);
-
-// Création du nom du produit dans la balise divDescription
-    const h2Name = document.createElement('h2');
-    h2Name.textContent = canape.name;
-    divDescription.appendChild(h2Name);
-
-// Création de la couleur du produit dans la balise divDescription
-    const pCouleur = document.createElement('p');
-    pCouleur.textContent = "Couleur : " + canape.color;
-    divDescription.appendChild(pCouleur);
-
-// Création du prix du produit dans la balise divDescription
-    const pPrix = document.createElement('p');
-    pPrix.textContent = "Prix : " + canape.price + " €";
-    divDescription.appendChild(pPrix);
-
-// Création d'une Div qui contient la quantité, de 1-100 produits et la suppression
-    const divQuantiteSupp = document.createElement('div');
-    divQuantiteSupp.textContent = "cart__item__content__settings";
-    divGroupe.appendChild(divQuantiteSupp);
-
-// Création d'une Div pour la quantité et le nombre
-    const divQuantite = document.createElement('div');
-    divQuantite.className = "cart__item__content__settings__quantity";
-    divQuantiteSupp.appendChild(divQuantite);
-
-// Création d'une balise p pour la quantité
-    const pQuantite = document.createElement('p');
-    pQuantite.textContent = "Qté : ";
-    divQuantite.appendChild(pQuantite);
-
-// Création d'un Input pour la gestion de la quantité
-    const inputQuantite = document.createElement('input');
-    inputQuantite.className = 'itemQuantity';
-    inputQuantite.setAttribute('type','number');
-    inputQuantite.setAttribute('name','itemQuantity');
-    inputQuantite.setAttribute('min','1');
-    inputQuantite.setAttribute('max','100');
-    inputQuantite.setAttribute('value', canape.quantity);
-    divQuantite.appendChild(inputQuantite);
-
-// Création d'une Div pour la suppression
-    const divSupp = document.createElement('div');
-    divSupp.className = "cart__item__content__settings__delete";
-    divQuantite.appendChild(divSupp);
-
-// Création d'une balise p pour supprimer
-    const pSupp = document.createElement('p');
-    pSupp.className = "deleteItem";
-    pSupp.textContent = "Supprimer";
-    divSupp.appendChild(pSupp);
-}
 
 
 
 
 
 
-const articleCanape = montreArticle(obj);
-    positionArticleCanape(articleCanape);
-    console.log(articleCanape)
-    
-    const divImg = montreImage(obj);
-    articleCanape.appendChild(divImg);
 
-    const divGrandContenu = montreContenuDescriptionParams(obj);
-    articleCanape.appendChild(divGrandContenu);
-}
+// Formulaire
 
-// Fonction création div regroupant la description et les parametres
-function montreDivGrandContenu() {
-    const divGroupe = document.createElement('div');
-    divGroupe.classList.add('cart__item__content');
-}
-
-// Fonction création div avec description et div avec parametre
-function montreContenuDescriptionParams(obj) {
-    const description = montreDescription(obj);
-    const parametre = montreParams(obj);
-}
-
-// Fonction création div avec parametre
-function montreParams(obj) {}
-
-// Fonction création div avec description
-function montreDescription(obj) {
-    const divGroupe = document.createElement('div');
-    divDescription.classList.add('cart__item__content__description');
-
-    const h2Name = document.createElement('h2');
-    h2Name.textContent = obj.name;
-
-    const pCouleur = document.createElement('p');
-    pCouleur.textContent = obj.color;
-
-    divGroupe.appendChild(divDescription);
-    divDescription.appendChild(h2Name);
-    divDescription.appendChild(pCouleur);
-    return description;
-}
-
-// Fonction position de l'article
-function positionArticleCanape(articleCanape) {
-    document.querySelector('#cart__items').appendChild(articleCanape);
-}
-
-// Fonction création de l'article
-function montreArticle(obj) {
-    const articleCanape = document.createElement('article');
-    articleCanape.classList.add('cart__item');
-    articleCanape.dataset.id = obj.id;
-    articleCanape.dataset.color = obj.color;
-    return articleCanape;
-}
-
-// Fonction création de l'image dans une div
-function montreImage(obj) {
-    const divImg = document.createElement('div');
-    divImg.classList.add('cart__item__img');
-
-    const imgCanape = document.createElement('img');
-    imgCanape.src = obj.image;
-    imgCanape.alt = obj.altImg;
-    divImg.appendChild(imgCanape);
-    return divImg;
-}
-afficheDetail();*/
+const formulaire = document.querySelector(".cart__order__form");
